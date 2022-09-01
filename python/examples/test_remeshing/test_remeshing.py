00a36ca14f156278dfacc34fc2de655d8ae39e09
sunethwarna <suneth.warna@gmail.com>
2022-09-01 11:46:39 +0200

    add comment

--- python/examples/test_remeshing/test_remeshing.py
+++ python/examples/test_remeshing/test_remeshing.py
@@ -35,6 +35,12 @@ mmg_properties = Kratos.Parameters("""{
 def SetVariableValues(model_part):
     write_h5("before_refinement.h5", model_part)
 
+    # here set the PRESSURE variable for what ever the higher values where you want to see more refinement
+
+    # following is an example for blocking. When Blocked, those nodes will not be changed. You can block elements and conditions as well if you like.
+    # in this example, since I am not setting any PRESSURE values, it will try to remove as many nodes as possible in the un blocked regions
+    # because the gradients are 0.0 everywhere. So in order to have some specialized refinement, you have to have some values in nodes so
+    # that there will be some gradients.
     geometric_center = Kratos.Array3(0.0)
     refinement_model_part = model_part.GetSubModelPart("Slip2D.Slip2D_cylinder")
     refinement_influence_radius = 0.3
@@ -48,7 +54,7 @@ def SetVariableValues(model_part):
     for node in model_part.Nodes:
         distance = sqrt((node.X - geometric_center[0]) ** 2 + (node.Y - geometric_center[1]) ** 2 + (node.Z - geometric_center[2]) ** 2)
         # blocking refinement if the distance is greater than refinement_influence_radius
-        node.Set(Kratos.BLOCKED, distance > refinement_influence_radius)
+        node.Set(Kratos.BLOCKED, distance < refinement_influence_radius)
 
 refined_model_part = RefineMesh("cylinder", "modified_cylinder", mmg_properties, [Kratos.PRESSURE], SetVariableValues)
 write_h5("after_refinement.h5", refined_model_part)
