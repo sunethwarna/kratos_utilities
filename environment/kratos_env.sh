af16607c128b5a93d2ae0d38b73e7eebf825166c
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2025-12-19 23:24:46 +0100

    fix kearos env

--- environment/kratos_env.sh
+++ environment/kratos_env.sh
@@ -8,12 +8,12 @@ Help()
     echo
     echo " Syntax: sh kratos_env.sh [options] [environment name] [[compiler] [build_mode] | [branch_name]]"
     echo "options:"
-    echo "          -h, --help   : Displays this help message."
-    echo "          -s, --summary: Displays summary of work trees."
-    echo "          -c, --create : Add a new branch with [branch_name] and create the work tree with [environment name]"
-    echo "          -a, --add    : Add a new work tree with [environment name] for branch with [branch_name]"
-    echo "          -u, --update : Update a  work tree with [environment name]"
-    echo "          -r, --remove : Remove [environment name] worktree."
+    echo "          -h, : Displays this help message."
+    echo "          -s, : Displays summary of work trees."
+    echo "          -c, : Add a new branch with [branch_name] and create the work tree with [environment name]"
+    echo "          -a, : Add a new work tree with [environment name] for branch with [branch_name]"
+    echo "          -u, : Update a  work tree with [environment name]"
+    echo "          -r, : Remove [environment name] worktree."
     echo "input arguments:"
     echo "            compiler: gcc, clang, intel"
     echo "          build_mode: release, rel_with_deb_info, debug, full_debug"
@@ -22,10 +22,8 @@ Help()
 
     echo "Path information:"
     echo "      Kratos work tree master path       : $KRATOS_WORKTREE_MASTER_PATH"
-    echo "      Python environment path            : $PYTHON_VENV_PATH"
     echo "      Kratos utilities path              : $KRATOS_ENV_SCRIPT_DIR"
     echo "      Kratos default configuration script: $KRATOS_CONFIGURATION_SCRIPT"
-    echo "      Python environment configuration   : $PYTHON_VENV_EXECUTABLE"
     echo
 
     current_path=$(pwd)
@@ -47,13 +45,21 @@ Help()
 
             "branch")
             worktree_branch=$(echo $item_1)
-            printf "\t%30s" $worktree_name
+            build_type="N/A"
+            if [ -f "$KRATOS_BASE_PATH/$worktree_name/.venv/.kratos_info" ]; then
+                source "$KRATOS_BASE_PATH/$worktree_name/.venv/.kratos_info"
+            fi
+            printf "\t%30s" "${worktree_name} ($build_type)"
             printf "\t%s\n" ${worktree_branch:11}
             ;;
 
             "detached")
             worktree_branch="detached[$worktree_hash]"
-            printf "\t%30s" $worktree_name
+            build_type="N/A"
+            if [ -f "$KRATOS_BASE_PATH/$worktree_name/.venv/.kratos_info" ]; then
+                source "$KRATOS_BASE_PATH/$worktree_name/.venv/.kratos_info"
+            fi
+            printf "\t%30s" "${worktree_name} ($build_type)"
             printf "\t%s\n" $worktree_branch
             ;;
 
@@ -71,13 +77,6 @@ Help()
     echo "      kratos_compile_clean: Cleans and compiles currently loaded kratos environment and re-initializes the environment"
     echo "    kratos_paraview_output: Creates xdmf file using the given h5 files for paraview visualization"
     echo "             kratos_unload: Unloads kratos environment"
-
-    if ! command -v virtualenv 2>&1 >/dev/null
-    then
-        echo "--------------------------------------------------------------------------------------------------------"
-        echo "${bold}Warning: virtualenv could not be found. Install python-virtualenv or virtualenv packages.${normal}"
-        echo "--------------------------------------------------------------------------------------------------------"
-    fi
 }
 
 CheckAndInitializeEnvironmentVariables()
@@ -102,16 +101,7 @@ CheckAndInitializeEnvironmentVariables()
             echo "--- Changed KRATOS_WORKTREE_MASTER_PATH to $KRATOS_WORKTREE_MASTER_PATH."
         fi
 
-        DEFAULT_PYTHON_VENV_PATH="/software/python_venv"
-        read "${prompt_prefix}Specify the kratos master path ($DEFAULT_PYTHON_VENV_PATH): " PYTHON_VENV_PATH
-        if [ -z $PYTHON_VENV_PATH ]; then
-            PYTHON_VENV_PATH=$DEFAULT_PYTHON_VENV_PATH
-            echo "--- Using default PYTHON_VENV_PATH = $PYTHON_VENV_PATH."
-        else
-            echo "--- Changed PYTHON_VENV_PATH to $PYTHON_VENV_PATH."
-        fi
-
-        DEFAULT_KRATOS_CONFIGURATION_SCRIPT="configure.sh"
+        DEFAULT_KRATOS_CONFIGURATION_SCRIPT="configure_zsh.sh"
         read "${prompt_prefix}Specify the kratos default configuration script name ($DEFAULT_KRATOS_CONFIGURATION_SCRIPT): " KRATOS_CONFIGURATION_SCRIPT
         if [ -z $KRATOS_CONFIGURATION_SCRIPT ]; then
             KRATOS_CONFIGURATION_SCRIPT=$DEFAULT_KRATOS_CONFIGURATION_SCRIPT
@@ -120,20 +110,10 @@ CheckAndInitializeEnvironmentVariables()
             echo "--- Changed KRATOS_CONFIGURATION_SCRIPT to $KRATOS_CONFIGURATION_SCRIPT."
         fi
 
-        DEFAULT_PYTHON_VENV_EXECUTABLE="virtualenv"
-        read "${prompt_prefix}Specify the default python environment configuration method ($DEFAULT_PYTHON_VENV_EXECUTABLE): " PYTHON_VENV_EXECUTABLE
-        if [ -z $PYTHON_VENV_EXECUTABLE ]; then
-            PYTHON_VENV_EXECUTABLE=$DEFAULT_PYTHON_VENV_EXECUTABLE
-            echo "--- Using default PYTHON_VENV_EXECUTABLE = $PYTHON_VENV_EXECUTABLE."
-        else
-            echo "--- Changed PYTHON_VENV_EXECUTABLE to $PYTHON_VENV_EXECUTABLE."
-        fi
-
         echo "#!/bin/$shell_type" >> $KRATOS_ENV_SCRIPT_DIR/kratos_environment_paths.sh
         echo "export KRATOS_WORKTREE_MASTER_PATH=\"$KRATOS_WORKTREE_MASTER_PATH\"" >> $KRATOS_ENV_SCRIPT_DIR/kratos_environment_paths.sh
-        echo "export PYTHON_VENV_PATH=\"$PYTHON_VENV_PATH\"" >> $KRATOS_ENV_SCRIPT_DIR/kratos_environment_paths.sh
         echo "export KRATOS_CONFIGURATION_SCRIPT=\"$KRATOS_CONFIGURATION_SCRIPT\"" >> $KRATOS_ENV_SCRIPT_DIR/kratos_environment_paths.sh
-        echo "export PYTHON_VENV_EXECUTABLE=\"$PYTHON_VENV_EXECUTABLE\"" >> $KRATOS_ENV_SCRIPT_DIR/kratos_environment_paths.sh
+        echo "export KRATOS_SHELL_TYPE=\"$shell_type\"" >> $KRATOS_ENV_SCRIPT_DIR/kratos_environment_paths.sh
     fi
     source $KRATOS_ENV_SCRIPT_DIR/kratos_environment_paths.sh
 }
@@ -222,165 +202,136 @@ CheckEnvironmentName()
     fi
 }
 
-ConfigureVariables()
-{
-    case $compiler_type in
-        "gcc")
-            ;;
-        "clang")
-            ;;
-        "intel")
-            ;;
-        *)
-            echo "-- Unsupported compiler type=\"$compiler_type\" provided. Followings are the valid compiler types: (use -h or --help to see full details)"
-            printf "\tgcc\n"
-            printf "\tclang\n"
-            printf "\tintel\n"
-            is_valid_options=false
-            ;;
-    esac
-
-    case $build_type in
-        "release")
-            export KRATOS_BUILD_TYPE="Release"
-            ;;
-        "rel_with_deb_info")
-            export KRATOS_BUILD_TYPE="RelWithDebInfo"
-            ;;
-        "debug")
-            export KRATOS_BUILD_TYPE="Debug"
-            ;;
-        "full_debug")
-            export KRATOS_BUILD_TYPE="FullDebug"
-            ;;
-        *)
-            echo "-- Unsupported build type=\"$build_type\" provided. Followings are the valid build types: (use -h or --help to see full details)"
-            printf "\trelease\n"
-            printf "\trel_with_deb_info\n"
-            printf "\tdebug\n"
-            printf "\tfull_debug\n"
-            is_valid_options=false
-            ;;
-    esac
-
-    export KRATOS_CPP_CONFIG_NAME=$(echo "${compiler_type}_${KRATOS_BUILD_TYPE}")
-}
-
-ReInitializeVirtualEnvironment()
-{
-    venv_name="$1"
-    venv_path=$PYTHON_VENV_PATH/$venv_name
-    if [ -d $venv_path ]; then
-        rm -r $venv_path
-    fi
-
-    python -m $PYTHON_VENV_EXECUTABLE $venv_path --system-site-packages
-}
-
-InitalizePythonVirtualEnvironment()
-{
-    venv_name="$1"
-    venv_path=$PYTHON_VENV_PATH/$venv_name
-    if [ ! -d $venv_path ]; then
-        ReInitializeVirtualEnvironment $venv_name
-    fi
-    source $PYTHON_VENV_PATH/$venv_name/bin/activate
-}
-
 CheckAndInitializeEnvironmentVariables
 export KRATOS_BASE_PATH=$(dirname $KRATOS_WORKTREE_MASTER_PATH)
 
-if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-    Help
-elif [ "$1" = "-s" ] || [ "$1" = "--summary" ]; then
-    Summary
-elif [ "$1" = "-r" ] || [ "$1" = "--remove" ]; then
-    environment_name=$2
-    CheckEnvironmentName
-    if $is_valid_options
-    then
-        current_path=$(pwd)
-        cd $KRATOS_WORKTREE_MASTER_PATH
-        git worktree remove $environment_name
-        cd $PYTHON_VENV_PATH
-        find . -type d -regextype egrep -regex ".\/${environment_name}_(gcc|intel|clang)_(Release|RelWithDebInfo|Debug|FullDebug)" | xargs -I{} rm -rf {}
-        cd $current_path
-    fi
-elif [ "$1" = "-c" ] || [ "$1" = "--create" ]; then
+current_path=$(pwd)
+
+KratosCompile()
+{
+    compiler=$1
+    build_type=$2
+
+    kratos_path=$(which python | xargs dirname | xargs dirname | xargs dirname)
+
     current_path=$(pwd)
-    cd $KRATOS_WORKTREE_MASTER_PATH
-    git checkout master
-    git pull
-    git checkout -b $3
-    git checkout master
-    git worktree add ../$2 $3
+    cd $kratos_path/scripts
+
+    case "$build_type" in
+        release)
+            unbuffer sh configure.sh ${compiler}_Release 2>&1 | tee kratos.compile.log ;;
+        debug)
+            unbuffer sh configure.sh ${compiler}_Debug 2>&1 | tee kratos.compile.log ;;
+        rel_with_deb_info)
+            unbuffer sh configure.sh ${compiler}_RelWithDebInfo 2>&1 | tee kratos.compile.log ;;
+        *)
+            echo "Unsupported build_type=\"$build_type\". Only supports following build types:"
+            printf "\trelease\n"
+            printf "\tdebug\n"
+            printf "\trel_with_deb_info\n"
+            ;;
+    esac
+
     cd $current_path
-elif [ "$1" = "-a" ] || [ "$1" = "--add" ]; then
-    current_path=$(pwd)
-    cd $KRATOS_WORKTREE_MASTER_PATH
-    git checkout master
-    git pull
-    git worktree add ../$2 $3
-    cd $current_path
-elif [ "$1" = "-u" ] || [ "$1" = "--update" ]; then
-    environment_name=$2
-    compiler_type=$3
-    build_type=$4
+}
 
-    CheckEnvironmentName
-
-    ConfigureVariables
-
-    if $is_valid_options
-    then
-        ReInitializeVirtualEnvironment ${environment_name}_${compiler_type}_${KRATOS_BUILD_TYPE}
-    fi
-else
-    environment_name=$1
-    compiler_type=$2
-    build_type=$3
-
-    CheckEnvironmentName
-
-    ConfigureVariables
-
-    if $is_valid_options
-    then
-        if [ ! -z "$VIRTUAL_ENV" ]; then
-            echo "-- Found already existing kratos environment initialization."
-            echo "-- Please use \"deactivate\" to deactivate existing environment and try again to load the new environment."
-        else
-            KRATOS_PATH=$KRATOS_BASE_PATH/$environment_name
-            InitalizePythonVirtualEnvironment ${environment_name}_${compiler_type}_${KRATOS_BUILD_TYPE}
-
-            if [ ! -f $KRATOS_PATH/scripts/configure.sh ]; then
-                echo "-- No default $KRATOS_PATH/scripts/configure.sh found. Copying the templated $KRATOS_ENV_SCRIPT_DIR/$KRATOS_CONFIGURATION_SCRIPT. file"
-                cp $KRATOS_ENV_SCRIPT_DIR/scripts/$KRATOS_CONFIGURATION_SCRIPT $KRATOS_PATH/scripts/configure.sh
-                sed -i "s/<KRATOS_NAME>/$environment_name/g" $KRATOS_PATH/scripts/configure.sh
-            fi
-
-            # now copy the rest of the default files if they are not found.
-            list_of_files_copied=$(cp -rvn $KRATOS_ENV_SCRIPT_DIR/defaults/. $KRATOS_PATH/)
-            if [ ! -z $list_of_files_copied ]; then
-                echo "-- Following files are copied..."
-                echo $list_of_files_copied
-            fi
-
-            alias kratos_compile='current_path=$(pwd) && cd $KRATOS_PATH/scripts && unbuffer sh configure.sh 2>&1 | tee kratos.compile.log && cd $current_path || cd $current_path'
-            alias kratos_compile_clean='current_path=$(pwd) && rm -rf $KRATOS_PATH/build/$KRATOS_CPP_CONFIG_NAME $KRATOS_PATH/bin/$KRATOS_CPP_CONFIG_NAME cd $current_path || cd $current_path'
-            alias kratos_paraview_output='python $KRATOS_PATH/applications/HDF5Application/python_scripts/create_xdmf_file.py'
-
-            echo "Initialized kratos environment at $KRATOS_PATH successfully using $CC compiler with $KRATOS_BUILD_TYPE build type."
-            echo
-            echo "Following commands are available:"
-            echo "            kratos_compile: Compiles currently loaded kratos environment and re-initializes the environment"
-            echo "      kratos_compile_clean: Cleans compiles currently loaded kratos environment and re-initializes the environment"
-            echo "    kratos_paraview_output: Creates xdmf file using the given h5 files for paraview visualization"
-            echo "                deactivate: Unloads kratos environment"
+case "$1" in
+    -h)
+        Help
+        ;;
+    -s)
+        Summary
+        ;;
+    -r)
+        environment_name=$2
+        CheckEnvironmentName
+        if $is_valid_options; then
+            cd $KRATOS_WORKTREE_MASTER_PATH
+            git worktree remove $environment_name
+            cd $current_path
         fi
-    fi
-fi
+        ;;
+    -c)
+        cd $KRATOS_WORKTREE_MASTER_PATH
+        git checkout master
+        git pull
+        git checkout -b $3
+        git checkout master
+        git worktree add ../$2 $3
+        python -m venv ../$2/.venv --system-site-packages
+        cd $current_path
+        ;;
+    -a)
+        cd $KRATOS_WORKTREE_MASTER_PATH
+        git checkout master
+        git pull
+        git worktree add ../$2 $3
+        python -m venv ../$2/.venv --system-site-packages
+        cd $current_path
+        ;;
+    -u)
+        environment_name=$2
+        CheckEnvironmentName
 
+        if $is_valid_options; then
+            python -m venv $KRATOS_BASE_PATH/$environment_name/.venv --system-site-packages
+        fi
+        ;;
+    *)
+        environment_name=$1
+        CheckEnvironmentName
 
+        if $is_valid_options; then
+            if [ ! -z "$VIRTUAL_ENV" ]; then
+                echo "-- Found already existing kratos environment initialization."
+                echo "-- Please use \"deactivate\" to deactivate existing environment and try again to load the new environment."
+            else
+                KRATOS_PATH=$KRATOS_BASE_PATH/$environment_name
 
+                if [ ! -f "$KRATOS_PATH/.venv/bin/activate" ]; then
+                    python -m venv $KRATOS_PATH/.venv --system-site-packages
+                    echo "-- Created python venv at $KRATOS_PATH/.venv"
+                fi
+                source $KRATOS_PATH/.venv/bin/activate
+                kratos_install_dir=$(python -c 'import site; print(site.getsitepackages()[0])')
+                python_interpreter=$(which python)
 
+                build_type="N/A"
+                if [ -f "$KRATOS_PATH/.venv/.kratos_info" ]; then
+                    source "$KRATOS_PATH/.venv/.kratos_info"
+                fi
+
+                if [ ! -f $KRATOS_PATH/scripts/configure.sh ]; then
+                    echo "-- No default $KRATOS_PATH/scripts/configure.sh found. Copying the templated $KRATOS_ENV_SCRIPT_DIR/$KRATOS_CONFIGURATION_SCRIPT. file"
+                    cp $KRATOS_ENV_SCRIPT_DIR/scripts/$KRATOS_CONFIGURATION_SCRIPT $KRATOS_PATH/scripts/configure.sh
+                fi
+
+                # now copy the rest of the default files if they are not found.
+                cp -rvn $KRATOS_ENV_SCRIPT_DIR/defaults/. $KRATOS_PATH/.temp/
+
+                find $KRATOS_PATH/.temp/ -type f -exec sed -i "s/<SHELL_TYPE>/${KRATOS_SHELL_TYPE}/g" {} +
+                find $KRATOS_PATH/.temp/ -type f -exec sed -i "s@<KRATOS_LIBS_DIR>@${kratos_install_dir}/libs@g" {} +
+                find $KRATOS_PATH/.temp/ -type f -exec sed -i "s@<PYTHON_INTERPRETER>@${python_interpreter}@g" {} +
+
+                list_of_files_copied=$(cp -rvn $KRATOS_PATH/.temp/. $KRATOS_PATH/)
+                if [ -d "$KRATOS_PATH/.temp" ]; then
+                    rm -r $KRATOS_PATH/.temp
+                fi
+                if [ ! -z $list_of_files_copied ]; then
+                    echo "-- Following files are copied..."
+                    echo $list_of_files_copied
+                fi
+
+                alias kratos_compile='KratosCompile'
+                alias kratos_paraview_output='python $KRATOS_PATH/applications/HDF5Application/python_scripts/create_xdmf_file.py'
+
+                echo "Initialized kratos environment at $KRATOS_PATH successfully with $build_type."
+                echo
+                echo "Following commands are available:"
+                echo "            kratos_compile: Compiles kratos with specified compiler (gcc|clang|intel) and with specified build type (release|debug|rel_with_deb_info)"
+                echo "    kratos_paraview_output: Creates xdmf file using the given h5 files for paraview visualization"
+                echo "                deactivate: Unloads kratos environment"
+            fi
+        fi
+        ;;
+esac
