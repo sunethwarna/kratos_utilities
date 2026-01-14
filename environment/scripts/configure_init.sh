2001e88efe8753889f948b9876e1bbfe99216b36
Suneth Warnakulasuriya <suneth.warna@gmail.com>
2026-01-14 13:12:46 +0100

    update configure files

---
+++ environment/scripts/configure_init.sh
@@ -0,0 +1,64 @@
+#!/bin/zsh
+
+#### This script should be only executed from the "scripts" folder of Kratos. ###
+
+python_venv_path=<PYTHON_VENV_PATH>
+kratos_name=$(pwd | rev | cut -d"/" -f2 | rev)
+
+RED='\033[0;31m'
+RESET='\033[0m'
+
+if [ ! -z "$VIRTUAL_ENV" ]; then
+    if [ ! -z "$1" ]; then
+        echo "-- Found already existing kratos environment initialization, therefore ignoring any input cpp configuration names given [ cpp_configuration_name = \"$1\" ]."
+    fi
+    python_venv_name=$(which python | rev | cut -d"/" -f3 | rev)
+
+    python_venv_kratos_name=$(echo $python_venv_name | cut -d"_" -f1)
+    if [[ "$kratos_name" != "$python_venv_kratos_name" ]]; then
+        echo -e "-- ${RED}Error: Initialized python environment (\"$python_venv_kratos_name\") and the kratos folder (\"$kratos_name\") mismatch.${RESET}"
+        exit
+    fi
+
+    # Set compiler
+    compiler_type=$(echo $python_venv_name | rev | cut -d"_" -f2 | rev)
+    export KRATOS_BUILD_TYPE=$(echo $python_venv_name | rev | cut -d"_" -f1 | rev)
+    export KRATOS_CPP_CONFIG_NAME="${compiler_type}_${KRATOS_BUILD_TYPE}"
+else
+    cpp_configuration_name=$1
+    if [ ! -d "$python_venv_path/${kratos_name}_${cpp_configuration_name}" ]; then
+        # if the python environment not found, then create it
+        python -m venv $python_venv_path/${kratos_name}_${cpp_configuration_name} --system-site-packages
+    fi
+    # activate the python environment
+    source $python_venv_path/${kratos_name}_${cpp_configuration_name}/bin/activate
+
+    # Set compiler
+    compiler_type=$(echo $cpp_configuration_name | rev | cut -d"_" -f2 | rev)
+    export KRATOS_BUILD_TYPE=$(echo $cpp_configuration_name | rev | cut -d"_" -f1 | rev)
+    export KRATOS_CPP_CONFIG_NAME="${compiler_type}_${KRATOS_BUILD_TYPE}"
+fi
+
+export KRATOS_INSTALL_DIR=$(python -c 'import site; print(site.getsitepackages()[0])')
+
+case $compiler_type in
+    "gcc")
+        export CC=gcc
+        export CXX=g++
+        ;;
+    "clang")
+        export CC=clang
+        export CXX=clang++
+        ;;
+    "intel")
+        export CC=icx
+        export CXX=icpx
+        ;;
+    *)
+        echo "-- Unsupported compiler type provided in environment variable KRATOS_CPP_CONFIG_NAME. [ cpp_configuration_name = \"$cpp_configuration_name\" ]. Followings are the accepted compiler types:"
+        printf "\tgcc\n"
+        printf "\tclang\n"
+        printf "\tintel\n"
+        exit
+        ;;
+esac
--- environment/scripts/configure_zsh.sh
+++ environment/scripts/configure_zsh.sh
@@ -2,15 +2,9 @@
 
 #### This script should be only executed from the "scripts" folder of Kratos. ###
 
-cpp_configuration_name=$1
+source ./configure_init.sh $1
 
-if [ ! -d "../.venv" ]; then
-    # if the python environment not found, then create it
-    python -m venv ../.venv --system-site-packages
-fi
-
-# activate the python environment
-source ../.venv/bin/activate
+echo "===== Compiling kratos using \"$CC\" and \"$CXX\" in \"$KRATOS_BUILD_TYPE\"."
 
 # activate intel mkl
 source /opt/intel/oneapi/setvars.sh intel64
@@ -19,37 +13,6 @@ source /opt/intel/oneapi/setvars.sh intel64
 add_app () {
     export KRATOS_APPLICATIONS="${KRATOS_APPLICATIONS}$1;"
 }
-
-kratos_install_dir=$(python -c 'import site; print(site.getsitepackages()[0])')
-
-# Set compiler
-export compiler_type=$(echo $cpp_configuration_name | rev | cut -d"_" -f2 | rev)
-export KRATOS_BUILD_TYPE=$(echo $cpp_configuration_name | rev | cut -d"_" -f1 | rev)
-export KRATOS_CPP_CONFIG_NAME="${compiler_type}_${KRATOS_BUILD_TYPE}"
-
-case $compiler_type in
-    "gcc")
-        export CC=gcc
-        export CXX=g++
-        ;;
-    "clang")
-        export CC=clang
-        export CXX=clang++
-        ;;
-    "intel")
-        export CC=icx
-        export CXX=icpx
-        ;;
-    *)
-        echo "-- Unsupported compiler type provided in environment variable KRATOS_CPP_CONFIG_NAME. [ cpp_configuration_name = \"$cpp_configuration_name\" ]. Followings are the accepted compiler types:"
-        printf "\tgcc\n"
-        printf "\tclang\n"
-        printf "\tintel\n"
-        exit
-        ;;
-esac
-
-echo "===== Compiling kratos using \"$compiler_type\" in \"$KRATOS_BUILD_TYPE\"."
 
 # Set variables
 export KRATOS_SOURCE="${KRATOS_SOURCE:-"$( cd "$(dirname "$0")" ; pwd -P )"/..}"
@@ -62,17 +25,17 @@ export PYTHON_EXECUTABLE=${PYTHON_EXECUTABLE:-"python"}
 
 # Set applications to compile
 export KRATOS_APPLICATIONS=
-# add_app ${KRATOS_APP_DIR}/MedApplication
 # add_app ${KRATOS_APP_DIR}/StructuralMechanicsApplication
 # add_app ${KRATOS_APP_DIR}/ConstitutiveLawsApplication
+# add_app ${KRATOS_APP_DIR}/LinearSolversApplication
+# add_app ${KRATOS_APP_DIR}/OptimizationApplication
+# add_app ${KRATOS_APP_DIR}/MeshMovingApplication
+# add_app ${KRATOS_APP_DIR}/MedApplication
 # add_app ${KRATOS_APP_DIR}/HDF5Application
 # add_app ${KRATOS_APP_DIR}/MetisApplication
 # add_app ${KRATOS_APP_DIR}/TrilinosApplication
-# add_app ${KRATOS_APP_DIR}/LinearSolversApplication
 # add_app ${KRATOS_APP_DIR}/ShapeOptimizationApplication
-# add_app ${KRATOS_APP_DIR}/OptimizationApplication
 # add_app ${KRATOS_APP_DIR}/SystemIdentificationApplication
-# add_app ${KRATOS_APP_DIR}/MeshMovingApplication
 
 # Clean
 clear
@@ -82,7 +45,7 @@ rm -rf "${KRATOS_BUILD}/${KRATOS_CPP_CONFIG_NAME}/CMakeFiles"
 
 # Configure
 cmake -H"${KRATOS_SOURCE}" -B"${KRATOS_BUILD}/${KRATOS_CPP_CONFIG_NAME}"   \
--DCMAKE_INSTALL_PREFIX="${kratos_install_dir}"                             \
+-DCMAKE_INSTALL_PREFIX="${KRATOS_INSTALL_DIR}"                             \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON                                         \
 -DUSE_MPI=ON                                                               \
 -DCMAKE_CXX_COMPILER_LAUNCHER=ccache                                       \
@@ -96,10 +59,5 @@ cmake -H"${KRATOS_SOURCE}" -B"${KRATOS_BUILD}/${KRATOS_CPP_CONFIG_NAME}"   \
 # Buid
 cmake --build "${KRATOS_BUILD}/${KRATOS_CPP_CONFIG_NAME}" --target install -j10
 
-# clear the .kratos_info file
-if [ -f "../.venv/.kratos_info" ]; then
-    rm "../.venv/.kratos_info"
-fi
-
-echo "#!/bin/zsh" >> "../.venv/.kratos_info"
-echo "export build_type=\"$KRATOS_CPP_CONFIG_NAME\"" >> "../.venv/.kratos_info"
+# link the cpp tests
+ln -sf ${KRATOS_INSTALL_DIR}/test "${KRATOS_BUILD}/${KRATOS_CPP_CONFIG_NAME}/test"
